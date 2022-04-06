560b256891fc5aca1794d69c19c1f7a9b6b87871
Cyrus Wong <cywong@vtc.edu.hk>
2022-04-06 06:34:07 +0000

    Add control form save and reload logic.

--- server.py
+++ server.py
@@ -13,12 +13,11 @@ import re
 import time
 from pathlib import Path
 
-class Server(BaseHTTPRequestHandler):
 
-        
+class Server(BaseHTTPRequestHandler):
     def _set_response(self):
         self.send_response(200)
-        self.send_header('Content-type', 'text/html')
+        self.send_header("Content-type", "text/html")
         self.end_headers()
 
     def do_GET(self):
@@ -27,7 +26,7 @@ class Server(BaseHTTPRequestHandler):
         this_dir = Path(__file__).resolve().parent
         if self.path == "/":
             filepath = this_dir / "output/grading_form" / "index.html"
-        else:            
+        else:
             filepath = this_dir / "output/grading_form" / self.path[1:]
         mimetype, _ = mimetypes.guess_type(str(filepath))
         self.send_header("Content-type", mimetype)
@@ -40,43 +39,64 @@ class Server(BaseHTTPRequestHandler):
         return
 
     def do_POST(self):
-        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
-        post_data = self.rfile.read(content_length) # <--- Gets the data itself
-        result = re.search('\/questions\/(.*)\/index.html', self.path)
-        question = result.group(1)        
+        content_length = int(
+            self.headers["Content-Length"]
+        )  # <--- Gets the size of data
+        post_data = self.rfile.read(content_length)  # <--- Gets the data itself
+        result = re.search("\/questions\/(.*)\/index.html", self.path)
+        question = result.group(1)
         this_dir = Path(__file__).resolve().parent
-        filepath = this_dir / "output/grading_form/questions/" / question / "mark.json"
-        f = open(filepath, "w")
-        f.write(post_data.decode('utf-8'))
-        f.close()
+
+        data = json.loads(post_data.decode("utf-8"))
+        print(data)
+        prefix = ""
+        if data["type"] == "mark":
+            prefix = "mark"
+        elif data["type"] == "control":
+            prefix = "control"
+        filepath = (
+            this_dir / "output/grading_form/questions/" / question / (prefix + ".json")
+        )
         
+        json_str = json.dumps(data['data'])
+        f = open(filepath, "w")
+        f.write(json_str)
+        f.close()
+
         timestr = time.strftime("%Y%m%d-%H%M%S")
-        filepath = this_dir / "output/grading_form/questions/" / question / ("mark-" + timestr + ".json")
+        filepath = (
+            this_dir
+            / "output/grading_form/questions/"
+            / question
+            / (prefix + "-" + timestr + ".json")
+        )
         f = open(filepath, "w")
-        f.write(post_data.decode('utf-8'))
+        f.write(json_str)
         f.close()
-        
-        logging.info("Saveed: " + str(filepath));
-        
+
+        logging.info("Saveed: " + str(filepath))
+
         self._set_response()
         self.wfile.write(post_data)
-        
-    def log_message(self, format, *args):
-        return        
 
-def run(server_class=HTTPServer, handler_class=Server, port=8080):    
-    logging.basicConfig(filename='output/server.log', filemode='w', level=logging.DEBUG)
-    server_address = ('', port)
+    def log_message(self, format, *args):
+        return
+
+
+def run(server_class=HTTPServer, handler_class=Server, port=8080):
+    logging.basicConfig(filename="output/server.log", filemode="w", level=logging.DEBUG)
+    server_address = ("", port)
     httpd = server_class(server_address, handler_class)
-    logging.info('Starting httpd...\n')
+    logging.info("Starting httpd...\n")
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
         pass
     httpd.server_close()
-    logging.info('Stopping httpd...\n')
+    logging.info("Stopping httpd...\n")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     from sys import argv
 
     if len(argv) == 2:
