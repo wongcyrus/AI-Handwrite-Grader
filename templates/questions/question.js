04e359e5dae811540de0ddb3db18b46773bd71ca
Cyrus <6193567+wongcyrus@users.noreply.github.com>
2023-07-13 04:39:09 +0000

    Completed marking form generation

--- templates/questions/question.js
+++ templates/questions/question.js
@@ -2,10 +2,10 @@ const boundingBoxes = [
 {% for index, row in dataTable.iterrows() -%}     
 {
     'id': {{row["RowNumber"]}},
-    'left':{{row["BoundingBox"]["left"]}},
-    'top': {{row["BoundingBox"]["top"]}},
-    'width': {{row["BoundingBox"]["width"]}},
-    'height': {{row["BoundingBox"]["height"]}}
+    'left':{{row["left"]}},
+    'top': {{row["top"]}},
+    'width': {{row["width"]}},
+    'height': {{row["height"]}}
 },        
 {% endfor %}
 ];
